8515fdc271c1549cb1137fc59e94e32ee32c9dab
Lucian Buzzo <lucian@Lucians-iMac-2.local>
2015-11-24 20:30:40 +0000

    Working on template value replacement.

--- src/index.f95
+++ src/index.f95
@@ -1,24 +1,25 @@
 program echostd
+  use String_Functions
   use templates
   use, intrinsic :: iso_fortran_env, only: iostat_end
   implicit none
 
   character(len=30) :: path
-  character(len=30) :: file_name = 'build/templates/main.html'
+  character(len=30) :: file_name = 'build/templates/layout.html'
   character(len=999999) :: template
   character (len=999):: line
   integer :: io_status
 
   call getarg(1, path)
 
-  print *, 'The path requested is ', path
-
-  file_name = get_template(path)
+  template = get_template(path)
 
   open (20, file=file_name, status='unknown', iostat=io_status)
 
   do
     read (20, '(A)', iostat=io_status) line ! read line from input file
+    line = Replace_Text(line, '{{title}}', path)
+    line = Replace_Text(line, '{{content}}', '<p>The path requested is '//path//'</p>')
     write(*, '(999A)') line        ! write line to the screen
     if(io_status == iostat_end) exit
     if(io_status > 0) error stop '*** Error occurred while reading file. ***'
