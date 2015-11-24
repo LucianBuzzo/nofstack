57cb5c3d239984cc64f4ec5359029ccf53edbe59
Lucian Buzzo <lucian@Lucians-iMac-2.local>
2015-11-24 22:06:43 +0000

    File read has been moved to its own module.

--- src/index.f95
+++ src/index.f95
@@ -1,35 +1,20 @@
 program echostd
   use String_Functions
   use templates
-  use, intrinsic :: iso_fortran_env, only: iostat_end
+  use readfile
   implicit none
 
   character(len=30) :: path
   character(len=30) :: file_name = 'build/templates/layout.html'
-  character(len=999999) :: template
-  character (len=999):: line
-  character(:), allocatable :: response_text
-
-  integer :: io_status
-  integer :: size
+  character(len=999999) :: template, response_text
 
   call getarg(1, path)
 
-  response_text = ''
+  response_text = read_file(file_name)
   template = get_template(path)
 
-  open (20, file=file_name, status='unknown', iostat=io_status)
-
-  do
-    read (20, '(A)', iostat=io_status, advance='no', size=size) line ! read line from input file
-    line = Replace_Text(line, '{{title}}', path)
-    line = Replace_Text(line, '{{content}}', '<p>The path requested is '//path//'</p>')
-    response_text = response_text//trim(line)//NEW_LINE('A')
-    !write(*, *) trim(line)        ! write line to the screen
-    if(io_status == iostat_end) exit
-    if(io_status > 0) error stop '*** Error occurred while reading file. ***'
-  end do
-
+  response_text = Replace_Text(response_text, '{{title}}', path)
+  response_text = Replace_Text(response_text, '{{content}}', '<p>The path requested is '//path//'</p>')
 
   write(*, *) response_text        ! write response to the screen
  !print *, response_text        ! write line to the screen
---
+++ src/readfile.f95
@@ -0,0 +1,27 @@
+module readfile
+  implicit none
+contains
+  function read_file(file_name) result(resp)
+    use, intrinsic :: iso_fortran_env, only: iostat_end
+    character (len=30) :: file_name
+    character (len=99):: line
+    character (len=999999), allocatable :: tmp
+    character (:), allocatable :: resp
+    integer :: io_status, size
+    open (20, file=file_name, status='unknown', iostat=io_status)
+
+    do
+      read (20, '(A)', iostat=io_status, advance='no', size=size) line ! read line from input file
+      tmp = trim(tmp)//trim(line)//NEW_LINE('A')
+      write(*, *) trim(line)        ! write line to the screen
+      if(io_status == iostat_end) exit
+      if(io_status > 0) error stop '*** Error occurred while reading file. ***'
+    end do
+
+    ! Allocate here to prevent a segfault
+    allocate(character (len=len(tmp)):: resp)
+
+    resp = tmp
+
+  end function
+end module readfile
